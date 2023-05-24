0dfd984cfb8ffc167287f491e0c89f963c6f677e
Coding-Jarl <senvisage.spam@gmail.com>
2023-05-24 21:17:00 +0200

    refactor: rearchitecture CSS (+add button premices)

--- src/components/Sample/index.tsx
+++ src/components/Sample/index.tsx
@@ -1,12 +1,45 @@
+'use client'
+import { useState } from 'react'
 import styles from './style.module.scss'
 
 export default function Sample() {
+  const [isChecked, setIsChecked] = useState(false)
+  const toggleCheck = () => {
+    setIsChecked(!isChecked)
+  }
   return (
     <div className={styles.Sample}>
-      <div className={[styles.square].join(' ')}>Owi</div>
-      <div className={[styles.circle].join(' ')}>Owi</div>
-      <div className={[styles.square].join(' ')}>Owi</div>
-      <div className={[styles.circle].join(' ')}>Owi</div>
+      <label>
+        <input type="checkbox" checked={isChecked} onClick={toggleCheck} />
+        <div
+          className={[
+            isChecked && styles.pressed,
+            styles.block,
+            styles.button,
+          ].join(' ')}
+        >
+          Owi
+        </div>
+      </label>
+      <hr />
+      <div className={styles.gallery}>
+        <div className={[styles.block].join(' ')}>Owi</div>
+        <div className={[styles.block, styles.circle].join(' ')}>Owi</div>
+        <div className={[styles.block, styles.withColorBG].join(' ')}>Owi</div>
+        <div
+          className={[styles.block, styles.circle, styles.withColorBG].join(
+            ' '
+          )}
+        >
+          Owi
+        </div>
+        <div className={[styles.block, styles.withImgBG].join(' ')}>Owi</div>
+        <div
+          className={[styles.block, styles.circle, styles.withImgBG].join(' ')}
+        >
+          Owi
+        </div>
+      </div>
     </div>
   )
 }
